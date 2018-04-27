38db1d394ad8dd6876b880660afd61374b15049b
jhonatan <jdsjhonatan@hotmail.com>
2018-04-26 21:32:34 -0300

    Added AutoMapping mapping profiles

---
+++ DomainDrivenDesign.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using DomainDrivenDesign.Domain.Entities;
+using DomainDrivenDesign.MVC.ViewModels;
+
+namespace DomainDrivenDesign.MVC.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Client, ClientViewModel>();
+            CreateMap<Product, ProductViewModel>();
+        }
+
+    }
+}
---
+++ DomainDrivenDesign.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using DomainDrivenDesign.Domain.Entities;
+using DomainDrivenDesign.MVC.ViewModels;
+
+namespace DomainDrivenDesign.MVC.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<ProductViewModel, Product>();
+            CreateMap<ClientViewModel, Client>();
+
+
+        }
+    }
+}
